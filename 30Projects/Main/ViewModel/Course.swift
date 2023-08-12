c5464370bb5b57fc71228bf816b52b095e03150a
codelab21 <codelab21@outlook.com>
2023-08-12 19:54:07 +0300

    Project-1 WeatherApp

---
+++ 30Projects/Main/ViewModel/Course.swift
@@ -0,0 +1,26 @@
+//
+//  Course.swift
+//  SwiftUICourse
+//
+//  MARK: codelab21 follow me on Instagram and YouTube
+//
+
+import Foundation
+import SwiftUI
+
+class CourseViewModel: ObservableObject {
+    struct CourseTutorial {
+        let id = UUID()
+        let name: String
+        let destinationView: AnyView
+        
+        init(name: String, destinationView: AnyView) {
+            self.name = name
+            self.destinationView = destinationView
+        }
+    }
+    
+    let CourseTutorials: [CourseTutorial] = [
+        CourseTutorial(name: "Weather", destinationView: AnyView(WeatherView()))
+    ]
+}
--- 30Projects/_0ProjectsApp.swift
+++ 30Projects/_0ProjectsApp.swift
@@ -2,7 +2,7 @@
 //  _0ProjectsApp.swift
 //  30Projects
 //
-//  Created by Eymen on 12.08.2023.
+//  MARK: codelab21 follow me on Instagram and YouTube
 //
 
 import SwiftUI
