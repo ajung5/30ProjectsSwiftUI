78e08cc488db02844bdabd7afe6ef3e5416dfe21
codelab21 <codelab21@outlook.com>
2023-08-12 19:51:12 +0300

    Initial Commit

---
+++ 30Projects/_0ProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0ProjectsApp.swift
+//  30Projects
+//
+//  Created by Eymen on 12.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct _0ProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
